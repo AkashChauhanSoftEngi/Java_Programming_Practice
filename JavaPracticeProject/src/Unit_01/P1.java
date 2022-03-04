c22b7dea2eabd11a64976623646dca4daca88de9
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2022-03-04 12:52:23 +0530

    Update P1.java

--- JavaPracticeProject/src/Unit_01/P1.java
+++ JavaPracticeProject/src/Unit_01/P1.java
@@ -4,8 +4,8 @@ public class P1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, World Program!");
-
+		System.out.println("Made changes in git through github!");
+		
 	}
 
 }
